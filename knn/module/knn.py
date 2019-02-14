6b94e75b48816d433dce1e3d2b03ad25e31960bf
James White <sirjwhite@gmail.com>
2019-02-13 20:40:52 -0600

    started to work

---
+++ knn/module/knn.py
@@ -0,0 +1,18 @@
+'''
+Created on Feb 13, 2019
+
+@author: sirjwhite
+'''
+
+class knn:
+    
+    
+    
+    def __init__(self):
+        print("init")
+    
+    def knn(self,data, targets, k, metric, inputs):
+        print("This is the dream")
+        
+    def euclidean(self):
+        print("euclidean")
---
+++ knn/module/testknn.py
@@ -0,0 +1,15 @@
+'''
+Created on Feb 13, 2019
+
+@author: sirjwhite
+'''
+
+from module.knn import knn
+
+if __name__ == '__main__':
+    X_train = [1,3,4]
+    y_train = [4,6,5]
+    X_test = [2,3,4]
+    k = knn()
+    print(k)
+    k.knn(X_train, y_train, knn.euclidean(k),5, X_test)
