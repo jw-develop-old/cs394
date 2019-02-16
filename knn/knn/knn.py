e9e8fb94091672ec0c6f95f2789f345778bc3d26
James White <jameswhite@cslab07.wheaton.edu>
2019-02-15 20:49:09 -0600

    yep

---
+++ knn/knn/knn.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 13, 2019
+
+@author: sirjwhite
+'''
+
+class knn:
+    def __init__(self,data, targets, k, metric, inputs):
+        print("This is the dream")
+
+class euclidean:
+    def euclidean(self):
+        print("euclidean")
+
+class manhattan:
+    def manhattan(self):
+        print("manhattan")
