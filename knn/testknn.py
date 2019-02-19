2b71eff5fdef2490e36c291d0e4a9f7b9bfc5319
James White <sirjwhite@gmail.com>
2019-02-19 16:50:13 -0600

    u

--- knn/testknn.py
+++ knn/testknn.py
@@ -7,17 +7,19 @@ Created on Feb 13, 2019
 import knn
 import numpy as np
 import csv
-
+import random
 
 from sklearn.datasets import load_iris, load_wine, load_digits
 from sklearn.model_selection import train_test_split
 
 def runTest(ts):
 
-	data, inputs, targets, y_test = train_test_split(
-	dataset['data'], dataset['target'], test_size=ts,random_state=0)
+	r = random.randrange(100)
 
-	k = 3
+	data, inputs, targets, y_test = train_test_split(
+	dataset['data'], dataset['target'], test_size=ts,random_state=r)
+
+	k = 2
 	model = knn.euclidean
 
 #	print("\nTraining shape:", data.shape)
@@ -35,11 +37,11 @@ if __name__ == '__main__':
 
 	datasets = [load_iris(),load_wine(),load_digits()]
 	dataset = load_iris()
-	val = .05
+	val = .001
 
 	fnames = ['test_size','percent']
 	w = csv.DictWriter(open('test_results.csv', 'w', newline=''), delimiter=',',
 							quotechar='|', quoting=csv.QUOTE_NONE,fieldnames=fnames)
 	w.writeheader()
-	for ts in np.arange(val,1,val):
+	for ts in np.arange(val,1-val,val):
 		w.writerow({'test_size' : "{:.2f}".format(ts),'percent' : "{:.2f}".format(runTest(ts))})
