e8ed363d6218e52a1e562edda4140813bb1498c6
James White <sirjwhite@gmail.com>
2019-02-19 16:34:00 -0600

    u

--- knn/testknn.py
+++ knn/testknn.py
@@ -12,39 +12,33 @@ import csv
 from sklearn.datasets import load_iris, load_wine, load_digits
 from sklearn.model_selection import train_test_split
 
-def write(*items):
-    w = csv.writer(open('knn_test_results', 'w', newline=''), delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for item in items:
-    	w.writerow(item)
-
 def runTest(ts):
 
-    data, inputs, targets, y_test = train_test_split(
-    dataset['data'], dataset['target'], test_size=ts,random_state=0)
+	data, inputs, targets, y_test = train_test_split(
+	dataset['data'], dataset['target'], test_size=ts,random_state=0)
 
-    k = 3
-    model = knn.euclidean
+	k = 3
+	model = knn.euclidean
 
-#    print("\nTraining shape:", data.shape)
-#    print("Testing shape:", inputs.shape)
+#	print("\nTraining shape:", data.shape)
+#	print("Testing shape:", inputs.shape)
 
-    y_pred = knn.knn(data, targets,k,model, inputs)
-    y_cheat = knn.illegal(data, targets,k,model, inputs)
+	y_pred = knn.knn(data, targets,k,model, inputs)
+	y_cheat = knn.illegal(data, targets,k,model, inputs)
 
-    percent = 1 - np.mean(y_pred != y_test)
-    print("\nTest knn percent: {:.2f}".format(percent))
+	percent = 1 - np.mean(y_pred != y_test)
+	print("\nTest knn percent: {:.2f}".format(percent))
 
-    return percent
+	return percent
 
 if __name__ == '__main__':
 
-    datasets = [load_iris(),load_wine(),load_digits()]
-    dataset = load_iris()
+	datasets = [load_iris(),load_wine(),load_digits()]
+	dataset = load_iris()
 
-    toWrite = [[" ","Percentage"]]
-
-    for ts in np.arange(.05,.95,.05):
-    	toWrite.append([ts,runTest(ts)])
-
-    write(toWrite)
+	fnames = ['test_size','percent']
+	w = csv.DictWriter(open('knn_test_results', 'w', newline=''), delimiter=',',
+							quotechar='|', quoting=csv.QUOTE_NONE,fieldnames=fnames)
+	w.writeheader()
+	for ts in np.arange(.05,.95,.05):
+		w.writerow({'test_size' : ts,'percent' : runTest(ts)})
