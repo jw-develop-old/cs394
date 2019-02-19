704391ae6ff1fce72143001be360c7166a9f273d
James White <sirjwhite@gmail.com>
2019-02-19 16:08:30 -0600

    u

--- knn/testknn.py
+++ knn/testknn.py
@@ -13,10 +13,10 @@ from sklearn.datasets import load_iris, load_wine, load_digits
 from sklearn.model_selection import train_test_split
 
 def write(*items):
-	with open('knn_test_results', 'w', newline='') as csvfile:
-	    righter = csv.writer(csvfile, delimiter=' ')
-	    for item in items:
-	    	righter.writerow(item)
+    w = csv.writer(open('knn_test_results', 'w', newline=''), delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for item in items:
+    	w.writerow(item)
 
 def runTest(ts):
 
@@ -26,23 +26,25 @@ def runTest(ts):
     k = 3
     model = knn.euclidean
 
-    print("\nTraining shape:", data.shape)
-    print("Testing shape:", inputs.shape)
+#    print("\nTraining shape:", data.shape)
+#    print("Testing shape:", inputs.shape)
 
     y_pred = knn.knn(data, targets,k,model, inputs)
     y_cheat = knn.illegal(data, targets,k,model, inputs)
 
-    print("\nTest knn percent: {:.2f}".format(1 - np.mean(y_pred != y_test)))
-    print("Illegal knn percent: {:.2f}\n".format(1 - np.mean(y_cheat != y_test)))
+    percent = 1 - np.mean(y_pred != y_test)
+    print("\nTest knn percent: {:.2f}".format(percent))
+
+    return percent
 
 if __name__ == '__main__':
 
     datasets = [load_iris(),load_wine(),load_digits()]
     dataset = load_iris()
 
-    toWrite[][] = [][]
-    toWrite[0] = [["Okay"],["Frick yeah"],["For sure"]
+    toWrite = [[" ","Percentage"]]
 
     for ts in np.arange(.05,.95,.05):
-    	runTest(ts)
+    	toWrite.append([ts,runTest(ts)])
 
+    write(toWrite)
